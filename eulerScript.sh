c23f962fc9958447b5b4af2eddd062d591e2ba93
Kocsen Chung <kocsenc@gmail.com>
2013-02-26 18:10:01 -0500

    Prepping script for multiple folders/files

---
+++ eulerScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#count = "6"
+
+#while [ $count -lt 16 ] 
+#do
+#	echo Creating folder number $count
+#	mkdir "p$count"
+#	touch "./p$count/p$count.rb"
+#	count=$[$count + 1]
+#done
+
+
+for i in {6..16}; do
+	echo $i
+done
